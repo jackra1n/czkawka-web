9f79930c1980599e541fd7e04aaa3e1274bdd193
jackra1n <45038833+jackra1n@users.noreply.github.com>
2026-04-28 17:11:00 +0200

    feat: add progress stage label mapping and channel helpers

--- backend/src/scanners/mod.rs
+++ backend/src/scanners/mod.rs
@@ -2,9 +2,10 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 
+use czkawka_core::common::progress_data::{CurrentStage, ProgressData};
 use czkawka_core::common::tool_data::{CommonData, DeleteMethod};
 
-use crate::models::ScanRequest;
+use crate::models::{ScanProgress, ScanRequest, SharedProgress};
 
 pub mod bad_extensions;
 pub mod bad_names;
@@ -51,4 +52,83 @@ pub fn configure_common_data<T: CommonData>(tool: &mut T, request: &ScanRequest)
 
 pub fn make_stop_flag() -> Arc<AtomicBool> {
     Arc::new(AtomicBool::new(false))
+}
+
+pub fn stage_label(stage: CurrentStage) -> String {
+    match stage {
+        CurrentStage::CollectingFiles => "Collecting files",
+        CurrentStage::DeletingFiles => "Deleting files",
+        CurrentStage::RenamingFiles => "Renaming files",
+        CurrentStage::MovingFiles => "Moving files",
+        CurrentStage::HardlinkingFiles => "Creating hard links",
+        CurrentStage::SymlinkingFiles => "Creating symlinks",
+        CurrentStage::OptimizingVideos => "Optimizing videos",
+        CurrentStage::CleaningExif => "Cleaning EXIF data",
+
+        CurrentStage::DuplicateCacheSaving => "Saving cache",
+        CurrentStage::DuplicateCacheLoading => "Loading cache",
+        CurrentStage::DuplicatePreHashCacheSaving => "Saving pre-hash cache",
+        CurrentStage::DuplicatePreHashCacheLoading => "Loading pre-hash cache",
+        CurrentStage::DuplicateScanningName => "Scanning by name",
+        CurrentStage::DuplicateScanningSizeName => "Scanning by size and name",
+        CurrentStage::DuplicateScanningSize => "Scanning by size",
+        CurrentStage::DuplicateHidingHardLinks => "Hiding hard links",
+        CurrentStage::DuplicatePreHashing => "Pre-hashing files",
+        CurrentStage::DuplicateFullHashing => "Hashing files",
+
+        CurrentStage::SameMusicCacheSavingTags => "Saving tag cache",
+        CurrentStage::SameMusicCacheLoadingTags => "Loading tag cache",
+        CurrentStage::SameMusicCacheSavingFingerprints => "Saving fingerprint cache",
+        CurrentStage::SameMusicCacheLoadingFingerprints => "Loading fingerprint cache",
+        CurrentStage::SameMusicReadingTags => "Reading music tags",
+        CurrentStage::SameMusicCalculatingFingerprints => "Calculating fingerprints",
+        CurrentStage::SameMusicComparingTags => "Comparing tags",
+        CurrentStage::SameMusicComparingFingerprints => "Comparing fingerprints",
+
+        CurrentStage::SimilarImagesHidingHardLinks => "Hiding hard links",
+        CurrentStage::SimilarImagesCalculatingHashes => "Calculating image hashes",
+        CurrentStage::SimilarImagesComparingHashes => "Comparing image hashes",
+
+        CurrentStage::SimilarVideosHidingHardLinks => "Hiding hard links",
+        CurrentStage::SimilarVideosCalculatingHashes => "Calculating video hashes",
+        CurrentStage::SimilarVideosCreatingThumbnails => "Creating video thumbnails",
+
+        CurrentStage::BrokenFilesChecking => "Checking files",
+        CurrentStage::BadExtensionsChecking => "Checking extensions",
+        CurrentStage::BadNamesChecking => "Checking names",
+
+        CurrentStage::ExifRemoverCacheLoading => "Loading EXIF cache",
+        CurrentStage::ExifRemoverExtractingTags => "Extracting EXIF tags",
+        CurrentStage::ExifRemoverCacheSaving => "Saving EXIF cache",
+
+        CurrentStage::VideoOptimizerCreatingThumbnails => "Creating thumbnails",
+        CurrentStage::VideoOptimizerProcessingVideos => "Processing videos",
+    }
+    .to_string()
+}
+
+pub fn progress_to_scan_progress(data: &ProgressData) -> ScanProgress {
+    ScanProgress {
+        stage_label: stage_label(data.sstage),
+        current_stage_idx: data.current_stage_idx,
+        max_stage_idx: data.max_stage_idx,
+        entries_checked: data.entries_checked,
+        entries_to_check: data.entries_to_check,
+        bytes_checked: data.bytes_checked,
+        bytes_to_check: data.bytes_to_check,
+    }
+}
+
+pub fn spawn_progress_reader(
+    receiver: crossbeam_channel::Receiver<ProgressData>,
+    shared_progress: SharedProgress,
+) -> std::thread::JoinHandle<()> {
+    std::thread::spawn(move || {
+        while let Ok(data) = receiver.recv() {
+            let scan_progress = progress_to_scan_progress(&data);
+            if let Ok(mut progress) = shared_progress.lock() {
+                *progress = Some(scan_progress);
+            }
+        }
+    })
 }
